93fd52180ebf6b61118d17e76182d72c1e1988d9
Bingnan Li <leebingnam@hotmail.com>
2021-07-22 21:59:52 -0400

    Add Boids art assets

---
+++ Assets/Boids/Art Assets/TornadoBanditsStudio/Low Poly Background Bird/Scripts/Bird.cs
@@ -0,0 +1,209 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TornadoBanditsStudio.LowPolyBird
+{
+    /// <summary>
+    /// Bird class.
+    /// </summary>
+    public class Bird : MonoBehaviour
+    {
+        [Header ("Wings Settings")]
+        public Transform leftWing; //Left wing
+        public Transform rightWing; //Right wing
+
+        [Range (1, 60)]
+        public float wingAngle = 30; //Wing angle to reach before going back to 0
+
+        public float currentWingsRotationSpeed = 3; //the current wings rotation speed
+        public float minWingsRotationSpeed = 240; //min wings rotation speed
+        public float maxWingsRotationSpeed = 320; //max wings rotation speed
+
+        private bool rotateWingsBack; //is rotating wings back from the wing angle to 0
+
+        [Space (15)]
+        [Header ("Movement Settings")]
+        public float minSpeed = 0.5f; //min speed 
+        public float maxSpeed = 1.5f; //max speed
+        public float currentSpeed; //The speed chosen
+
+        public float currentRotationSpeed; //current rotation speed, chose between min and max rotation
+        public float minRotationSpeed = 0.1f; //min rotation speed
+        public float maxRotationSpeed = 0.5f; //max rotation speed
+
+        public bool enableGliding = true; //Would you like to make the bird plan when it goes down?
+
+
+        [Space (15)]
+        [Header ("Flock settings")]
+        public bool isPartOfFlock = false;  //Is this bird part of a flock?
+        public FlockManager flockManager; //reference to flock manager
+        public bool turningToTarget = false; //is the bird rotating to the target
+        public bool isGliding = false;
+
+
+
+        /// <summary>
+        /// Unity Start function.
+        /// </summary>
+        private void Start ()
+        {
+            //Set a random movement speed
+            ChangeCurrentSpeed ();
+
+            //We are starning with the wings at 0, 0, 0 euler angles.
+            rightWing.transform.localEulerAngles = leftWing.transform.localEulerAngles = Vector3.zero;
+            //We will start by rotating it to the chosen angle.
+            //When we hit the chosen angle we will go back 0 
+            rotateWingsBack = false;
+            //Get a random rotation speed.
+            currentWingsRotationSpeed = Mathf.Abs (Random.Range (minWingsRotationSpeed, maxWingsRotationSpeed));
+            //Set a random rotation speed between the chosen values
+            currentRotationSpeed = Random.Range (minRotationSpeed, maxRotationSpeed);
+        }
+
+        /// <summary>
+        /// Unity Update function.
+        /// </summary>
+        private void Update ()
+        {
+            //If this is used as a part of a flock
+            if (isPartOfFlock)
+            {
+                //If we are far away to the target then turn to the target
+                turningToTarget = (Vector3.Distance (transform.position, flockManager.flockTargetPosition) >= flockManager.minDistanceToTarget) ? true : false;
+
+                //Turn to the target
+                if (turningToTarget)
+                {
+                    Vector3 dir = flockManager.flockTargetPosition - transform.position;
+                    transform.rotation = Quaternion.Slerp (transform.rotation, Quaternion.LookRotation (dir), currentRotationSpeed * Time.deltaTime);
+                }
+                else
+                {
+                    //Trying to avoid some fps spikes, we will apply the rules for the flock randomly
+                    //It is not needed to be called once per frame.
+                    float randomFlockLogic = Random.Range (0, 30);
+                    if (randomFlockLogic == 5)
+                        ApplyFlockingRules ();
+                }
+
+                //The bird is always moving forward
+                this.transform.Translate (this.transform.forward * currentSpeed * Time.deltaTime, Space.World);
+
+            }
+
+            //If we let the bird plan if it goes down we won't rotate the wings anymore.
+            if (enableGliding)
+            {
+                //If the dot product of the bird's forward and the Up vector is less than -0.1f then is planning is true, which means that we won't have to rotate the wings
+                isGliding = Vector3.Dot (this.transform.forward, Vector3.up) < -0.1f;
+
+                //Rotate wings
+                if (isGliding == false)
+                    RotateWings ();
+            } else
+            {
+                RotateWings ();
+            }
+
+        }
+
+        /// <summary>
+        /// Rotate the wings
+        /// </summary>
+        private void RotateWings ()
+        {
+            //Get the current angle for one of the wing.
+            //We only need one of them
+            float currentAngle = GetCurrentAngle (this.rightWing.transform.localEulerAngles.z);
+
+            if (rotateWingsBack == false)
+            {
+                if (currentAngle > wingAngle)
+                {
+                    currentWingsRotationSpeed *= -1;
+                    rotateWingsBack = true;
+                }
+            }
+            else
+            {
+                //If we are rotating back to 0, check if we hit the 0 value.
+                //If it is 0 then go rotate to wing angle 
+                if (currentAngle < 0f)
+                {
+                    rotateWingsBack = false;
+                    currentWingsRotationSpeed *= -1;
+                }
+            }
+
+            //Rotate the wings.
+            //Don't forget that we are rotating the left wings to -wing angle
+            this.rightWing.Rotate (0, 0, currentWingsRotationSpeed * Time.deltaTime);
+            this.leftWing.Rotate (0, 0, -currentWingsRotationSpeed * Time.deltaTime);
+        }
+
+        /// <summary>
+        /// Get the current local euler angle, with - sign
+        /// </summary>
+        /// <param name="localEulerAngle"></param>
+        /// <returns></returns>
+        float GetCurrentAngle (float localEulerAngle)
+        {
+            return (localEulerAngle > 180 ? localEulerAngle - 360 : localEulerAngle);
+        }
+
+        /// <summary>
+        /// Change the speed
+        /// </summary>
+        private void ChangeCurrentSpeed ()
+        {
+            currentSpeed = Random.Range (minSpeed, maxSpeed);
+        }
+
+        /// <summary>
+        /// Apply flocking rules.
+        /// </summary>
+        void ApplyFlockingRules ()
+        {
+            //We are calculating the center of a group and make the current bird move to the center of the group
+            //The bird might be always face the avarage heading of the group
+            //The birds must avoid the closer neighbours
+
+            //To get the center we will add all positions of the birds and divede it with the count of birds
+            Vector3 groupCenter = Vector3.zero;
+            //The avoidance. 
+            Vector3 avoidanceVector = Vector3.zero;
+            //Distance between 2 birds
+            float distance;
+            //We will need the list of all the birds existent, that we have in the FlockManager 
+            for (int i = 0; i < flockManager.spawnedBirds.Count; i++)
+            {
+                Bird currentBird = flockManager.spawnedBirds[i];
+                //Don't do that for the current bird
+                if (currentBird != this)
+                {
+                    //Get the distance between the 2 birds
+                    distance = Vector3.Distance (this.transform.position, currentBird.transform.position);
+
+                    //If we are closer than 1 meter than add the diference between this position and the current bird position
+                    if (distance < 1f)
+                    {
+                        avoidanceVector += (this.transform.position - currentBird.transform.position);
+                    }
+                    //Add the bird to the group
+                    groupCenter += currentBird.transform.position;
+                }
+            }
+
+            //Divie the group center
+            groupCenter /= (flockManager.spawnedBirds.Count - 1);
+            //Set the direction based on target position, the group center and the avoidance
+            Vector3 direction = (groupCenter + flockManager.flockTargetPosition + avoidanceVector) - this.transform.position;
+            //Rotate the bird
+            if (direction != Vector3.zero)
+                transform.rotation = Quaternion.Slerp (transform.rotation, Quaternion.LookRotation (direction), currentRotationSpeed * Time.deltaTime);
+        }
+    }
+}
---
+++ Assets/Boids/Art Assets/TornadoBanditsStudio/Low Poly Background Bird/Scripts/FlockManager.cs
@@ -0,0 +1,130 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TornadoBanditsStudio.LowPolyBird
+{
+    /// <summary>
+    /// Flock manager class.
+    /// </summary>
+    public class FlockManager : MonoBehaviour
+    {
+        [Header ("Spawm Settings")]
+        public List<Bird> birdsPrefab = new List<Bird> (); //Birds prefabs
+        public int birdsNumber = 10; //Birds number
+        public Vector3 spawnMiddlePoint = Vector3.zero; //Spawn middle point
+        public Vector3 spawnBoundaries = Vector3.one; //Span boundaries
+        public List<Bird> spawnedBirds; //A list with the flock
+
+        public bool enableRandomScale = true; //Enable random scale
+        public float minScale = 0.8f; //Min scale
+        public float maxScale = 1.2f; //Max scale
+
+        private GameObject flockParent; //Just to keep the the birds in a single parent
+
+        [Space (15)]
+        [Header ("Flock Target Settings")]
+        public Vector3 flockTargetPosition = Vector3.zero; //target of flock
+        public float minDistanceToTarget = 2f; //the min distance between target and flock birds
+        private float initialDistance; //Initial distance
+        public Vector3 flockTargetBoxMiddlePoint = Vector3.zero; //the middle point of the flock target might go
+        public Vector3 flockTargetBoxSize = new Vector3 (25, 25, 25); //the box where the flock target might be
+
+        /// <summary>
+        /// Unity Start function.
+        /// </summary>
+        private void Start ()
+        {
+            //Make sure that the birds prefab list is not null
+            for (int i=0; i<birdsPrefab.Count; i++)
+            {
+                if (birdsPrefab[i] == null)
+                    birdsPrefab.RemoveAt (i);
+            }
+            
+            //Clear the spawned birds list
+            spawnedBirds.Clear ();
+
+            //Flock parent.
+            //An empty gameobject that contains all our birds in the flock
+            flockParent = new GameObject ("Flock");
+            //Spawn the birds
+            for (int i = 0; i < birdsNumber; i++)
+            {
+                //Get a random position
+                Vector3 randomPosition = GetRandomPosition (spawnMiddlePoint, spawnBoundaries);
+
+                //Spawn a new bird at a random position
+                Bird newBird = (Bird) Instantiate (birdsPrefab[(int) Random.Range (0, birdsPrefab.Count)]);
+
+                //If we enabled the scale, randomize the bird's scale
+                if (enableRandomScale)
+                {
+                    newBird.transform.localScale = Vector3.one * Random.Range (minScale, maxScale);
+                }
+
+                //Set the bird's flock manager and set it as a part of the flock
+                newBird.flockManager = this;
+                newBird.isPartOfFlock = true;
+
+                //Set the position and the parent
+                newBird.transform.position = randomPosition;
+                newBird.transform.SetParent (flockParent.transform, false);
+
+                //Add the spawned birds to our spawned birds list
+                spawnedBirds.Add (newBird);
+            }
+
+            //Initial min distance to the target. We will modify this value during update
+            initialDistance = minDistanceToTarget;
+            //Change the target position
+            ChangeTargetPosition ();
+        }
+
+        /// <summary>
+        /// Get a random position based on the boundaries set.
+        /// </summary>
+        /// <returns>Random position in boundaries.</returns>
+        private Vector3 GetRandomPosition (Vector3 center, Vector3 boundaries)
+        {
+            //Set a random position.
+            //We are spawning based on the middle point set in inspector and the boundaries of spawning.
+            return center + new Vector3 (Random.Range (-boundaries.x, boundaries.x), Random.Range (-boundaries.y, boundaries.y), Random.Range (-boundaries.z, boundaries.z)) / 2;
+        }
+
+        /// <summary>
+        /// Modifies target position
+        /// </summary>
+        private void ChangeTargetPosition ()
+        {
+            //Get a random position in our flock target box
+            flockTargetPosition = GetRandomPosition (flockTargetBoxMiddlePoint, flockTargetBoxSize);
+            //Set a random distance
+            minDistanceToTarget = Random.Range (initialDistance - initialDistance / 2, initialDistance + initialDistance / 2);
+            //Invoke this function again.
+            Invoke ("ChangeTargetPosition", Random.Range (10, 15));
+        }
+
+        /// <summary>
+        /// On draw gizmos.
+        /// </summary>
+        private void OnDrawGizmos ()
+        {
+#if UNITY_EDITOR
+            //Spawn settings visualised in the Editor
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireCube (spawnMiddlePoint, spawnBoundaries);
+
+            //Target box visualised in the Editor
+            Gizmos.color = Color.green;
+            Gizmos.DrawWireCube (flockTargetBoxMiddlePoint, flockTargetBoxSize);
+
+            if (UnityEditor.EditorApplication.isPlaying == true)
+            {
+                Gizmos.color = Color.cyan;
+                Gizmos.DrawSphere (flockTargetPosition, 0.1f);
+            }
+#endif
+        }
+    }
+}
